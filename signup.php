45016405ea5468e1682a887154b8aba10a5679b6
ca2roaldsoy <roaldsoyen@gmail.com>
2021-01-22 22:40:11 +0100

    added error message to user

--- signup.php
+++ signup.php
@@ -4,6 +4,29 @@
 
     <main>
       <h1>Sign Up</h1>
+      
+            <?php
+                // error messages
+                if(isset($_GET["error"])) {
+
+                    if($_GET["error"] == "emptyfields") {
+                        echo "<p> Fill in all fields</p>";
+                    }
+                    else if($_GET["error"] == "invalidmailuid") {
+                        echo "<p>invalid username and e-mail</p>";
+                    }
+                    else if($_GET["error"] == "invalidemail") {
+                        echo "<p>invalid email</p>";
+                    }
+                    else if($_GET["error"] == "passwordcheck") {
+                        echo "<p>password do not match</p>";
+                    }
+                    else if($_GET["error"] == "usertaken") {
+                        echo "<p>username already taken</p>";
+                    }
+                }
+            ?>
+
         <form action="includes/signup.inc.php" method="post">
             <input type="text" name="uid" placeholder="username">
             <input type="text" name="mail" placeholder="mail">
