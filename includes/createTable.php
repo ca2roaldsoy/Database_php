227f51f61b58739b3e326acb8fafdd531ca452f7
ca2roaldsoy <roaldsoyen@gmail.com>
2021-01-22 22:18:32 +0100

    create table

---
+++ includes/createTable.php
@@ -0,0 +1,19 @@
+<?php
+
+$db = new mysqli("localhost", "root", "", "test2000");
+
+mysqli_select_db($db,"test2000");
+
+$sql  = "CREATE TABLE IF NOT EXISTS allusers (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    uidUsers varchar(100) NOT NULL,
+    emailUsers varchar(100) NOT NULL,
+    pwdUsers varchar(100) NOT NULL
+    )";
+
+if ($db->query($sql)) {
+    echo "<p><b>Table created!</b></p>";
+    echo "<b>Sql:</b><pre>$sql</pre>";
+} else {
+    echo "<p>Error: something went wrong</p>";
+}
