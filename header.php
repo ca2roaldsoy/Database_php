ca50308d85aa9d0d488c6a8d5c80c031e5acd893
ca2roaldsoy <roaldsoyen@gmail.com>
2021-01-22 22:10:14 +0100

    started session

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,19 +12,19 @@
 <body>
 
 <header>
-<?php
-    if(isset($_SESSION["userId"])) {
-       echo '<form action="includes/logout.inc.php" method="post">
-                <button type="submit" name="logout-submit">Logout</button>
-            </form>';
-    }
-    else {
-        echo '<form action="includes/login.inc.php" method="post">
-                <input type="text" name="mailuid" placeholder="Email...">        
-                <input type="password" name="pwd" placeholder="Password...">
-                <button type="submit" name="login-submit">Login</button>
-            </form>
-            <a href="signup.php">Sign Up</a>';
-    }
-?>
+    <?php
+        if(isset($_SESSION["userId"])) {
+        echo '<form action="includes/logout.inc.php" method="post">
+                    <button type="submit" name="logout-submit">Logout</button>
+                </form>';
+        }
+        else {
+            echo '<form action="includes/login.inc.php" method="post">
+                    <input type="text" name="mailuid" placeholder="Email...">        
+                    <input type="password" name="pwd" placeholder="Password...">
+                    <button type="submit" name="login-submit">Login</button>
+                </form>
+                <a href="signup.php">Sign Up</a>';
+        }
+    ?>
 </header>
