d2481538fa8b9662e9c10d8c02cf46c6fdfa43c0
ca2roaldsoy <roaldsoyen@gmail.com>
2021-02-02 21:00:27 +0100

    removed logged in and logged out messages

--- index.php
+++ index.php
@@ -3,19 +3,6 @@ require "header.php";
 
 ?>
 
-<main class="container">
-    <?php
-     
-        if(!isset($_SESSION["userId"])) {
-            echo "<p>You are logged out!</p>";
-        }
-        else {
-            echo "<p>You are logged inn!</p>";
-        }
-
-    ?>
-    
-</main>
 
 <?php
     require "footer.php";
