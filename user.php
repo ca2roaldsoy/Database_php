5b9363373d5eaf97084653da2b19d9cc87f5dda8
ca2roaldsoy <roaldsoyen@gmail.com>
2021-02-02 20:59:51 +0100

    added username

--- user.php
+++ user.php
@@ -1,9 +1,14 @@
 <?php
 
-function user($userId, $userMail, $password)
+function user($userId, $userName, $userMail, $password)
 {
     echo "CREDENTIALS: <br /><br />";
     echo "id: $userId <br />";
+    echo "name: $userName <br />";
     echo "mail: $userMail <br />";
     echo "password: $password <br />";
+
+    echo '<form action="../index.php" method="POST">
+    <button type="submit" name="logout-submit">Logout</button>
+    </form>';
 }
