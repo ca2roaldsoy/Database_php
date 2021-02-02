1595cc3f6befaff57d3eace4759a65d4765f442b
ca2roaldsoy <roaldsoyen@gmail.com>
2021-02-02 22:05:39 +0100

    code cleanup

--- signup.php
+++ signup.php
@@ -28,10 +28,10 @@
             ?>
 
         <form action="includes/signup.inc.php" method="post" class="signup">
-            <input type="text" name="uid" placeholder="username">
-            <input type="text" name="mail" placeholder="mail">
-            <input type="password" name="pwd" placeholder="password">
-            <input type="password" name="pwd-repeat" placeholder="repeat password">
+            <input type="text" name="uid" placeholder="Username">
+            <input type="text" name="mail" placeholder="E-mail">
+            <input type="password" name="pwd" placeholder="Password">
+            <input type="password" name="pwd-repeat" placeholder="Repeat password">
             <button type="submit" name="signup-submit" class="btn btn-primary">Sign Up</button>
         </form>
     </main>
