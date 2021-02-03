87ec574815ad0a79a05c9fa40ca7d805f69faeef
ca2roaldsoy <roaldsoyen@gmail.com>
2021-02-03 14:02:01 +0100

    added main tag and btn class

--- user.php
+++ user.php
@@ -1,14 +1,21 @@
 <?php
 
+
 function user($userId, $userName, $userMail, $password)
 {
-    echo "CREDENTIALS: <br /><br />";
-    echo "id: $userId <br />";
-    echo "name: $userName <br />";
-    echo "mail: $userMail <br />";
-    echo "password: $password <br />";
+    echo "<main class='credentials'>";
+    echo "<h1>CREDENTIALS</h1>";
+    echo "<ul>";
+    echo "<li>ID: $userId</li>";
+    echo "<li>Name: $userName </li>";
+    echo "<li>E-Mail: $userMail </li>";
+    echo "<li>Password: $password </li>";
+    echo "</ul>";
+    
 
     echo '<form action="../index.php" method="POST">
-    <button type="submit" name="logout-submit">Logout</button>
+    <button type="submit" name="logout-submit" class="btn btn-primary">Logout</button>
     </form>';
+    
+    echo "</main>";
 }
