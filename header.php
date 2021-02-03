87f1abab22696bb5121f685f1d8273eaf4896b16
ca2roaldsoy <roaldsoyen@gmail.com>
2021-02-03 14:03:58 +0100

    moved loginform from here

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <?php
-    session_start();
-    require "./includes/createDatabase.php";
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -11,33 +10,15 @@
               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
               integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
               crossorigin="anonymous">
-              <link rel="stylesheet" href="style/style.css">
+            
+                <?php
+                if (isset($_POST["login-submit"])) {
+                        echo '<link rel="stylesheet" href="../style.css">';
+                    }
+                    else {
+                        echo '<link rel="stylesheet" href="style.css">';
+                    }
+              ?>
     <title>Document</title>
 </head>
 <body class="container">
-
-<header>
-    <h1>Log in</h1>
-    <?php
-
-        if(isset($_GET["error"])) {
-        
-            if($_GET["error"] == "emptyinputs") {
-                echo "<p class='error'> Fill in all fields</p>";
-            }
-            else if($_GET["error"] == "wrongpassword") {
-                echo "<p class='error'>Wrong password</p>";
-            }
-            else if($_GET["error"] == "nouserfound") {
-                echo "<p class='error'>Sorry, no user found</p>";
-            }
-
-        }
-            echo '<form action="includes/login.inc.php" method="post" class="login">
-                    <input type="text" name="mailuid" placeholder="Email...">        
-                    <input type="password" name="pwd" placeholder="Password...">
-                    <button type="submit" name="login-submit" class="btn btn-primary">Login</button>
-                </form>
-                <a href="signup.php">Sign Up</a>';
-    ?>
-</header>
